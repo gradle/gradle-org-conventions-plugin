389ecaca3212e00cda9fcc10c16259950da51c18
Bo Zhang <bo@gradle.com>
2020-06-29 22:22:59 +0800

    Fix publish key and secret

--- .teamcity/Project.kt
+++ .teamcity/Project.kt
@@ -67,15 +67,13 @@ object ReleasePlugin : BuildType({
     steps {
         gradle {
             useGradleWrapper = true
-            gradleParams = "-Dgradle.publish.skip.namespace.check=true -Pgradle.publish.key=%GRADLE_PUBLISH_KEY% -Pgradle.publish.secret=%GRADLE_PUBLISH_SECRET%"
+            gradleParams = "-Dgradle.publish.skip.namespace.check=true -Pgradle.publish.key=%plugin.portal.publish.key% -Pgradle.publish.secret=%plugin.portal.publish.secret%"
             tasks = "publishPlugins"
             buildFile = "build.gradle.kts"
         }
     }
     params {
         param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
-        password("GRADLE_PUBLISH_KEY", "credentialsJSON:8b35ff9d-0de0-4266-8850-9162aa31b4bf", display = jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay.HIDDEN)
-        password("GRADLE_PUBLISH_SECRET", "credentialsJSON:6934a5f0-e002-4067-884e-5090de3c9ec3", display = jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay.HIDDEN)
     }
 })
 
