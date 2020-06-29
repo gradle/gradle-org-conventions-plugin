68339d4637d7ac3cc1953be59fb65abfba0346ad
Bo Zhang <bo@gradle.com>
2020-06-29 22:07:21 +0800

    Config release

--- .teamcity/Project.kt
+++ .teamcity/Project.kt
@@ -1,3 +1,4 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.ParameterDisplay
 import jetbrains.buildServer.configs.kotlin.v2019_2.AbsoluteId
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
@@ -14,7 +15,7 @@ object Project : Project({
 object Verify : BuildType({
     id = AbsoluteId("VerifyGradleEnterpriseConventionsPlugin")
     uuid = "VerifyGradleEnterpriseConventionsPlugin"
-    name = "VerifyGradleEnterpriseConventionsPlugin"
+    name = "Verify Gradle Enterprise Conventions Plugin"
     description = "Verify Gradle Enterprise Conventions Plugin"
 
     vcs {
@@ -39,7 +40,7 @@ object Verify : BuildType({
 
     steps {
         gradle {
-            buildFile = "build.gradle.kts"
+            useGradleWrapper = true
             tasks = "check"
         }
     }
@@ -48,7 +49,7 @@ object Verify : BuildType({
 object ReleasePlugin : BuildType({
     id = AbsoluteId("ReleaseGradleEnterpriseConventionsPlugin")
     uuid = "ReleaseGradleEnterpriseConventionsPlugin"
-    name = "ReleaseGradleEnterpriseConventionsPlugin"
+    name = "Release Gradle Enterprise Conventions Plugin"
     description = "Release Gradle Enterprise Conventions Plugin"
 
     vcs {
@@ -60,6 +61,20 @@ object ReleasePlugin : BuildType({
 
     requirements {
         contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+
+    steps {
+        gradle {
+            useGradleWrapper = true
+            gradleParams = "-Dgradle.publish.skip.namespace.check=true -Pgradle.publish.key=%GRADLE_PUBLISH_KEY% -Pgradle.publish.secret=%GRADLE_PUBLISH_SECRET%"
+            tasks = "publishPlugins"
+        }
+    }
+
+    params {
+        param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
+        password("GRADLE_PUBLISH_KEY", "credentialsJSON:8b35ff9d-0de0-4266-8850-9162aa31b4bf", display = jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay.HIDDEN)
+        password("GRADLE_PUBLISH_SECRET", "credentialsJSON:6934a5f0-e002-4067-884e-5090de3c9ec3", display = jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay.HIDDEN)
     }
 })
 
