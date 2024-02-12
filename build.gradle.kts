7f3be7b62ec1be65022129c794c918f32988aef0
Mikhail Lopatkin <mlopatkin@gradle.com>
2024-02-12 15:28:28 +0100

    Use Gradle 7.6.4 as a new minimum requirement

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 rootProject.group = "com.github.gradle"
 rootProject.group = "io.github.gradle"
-rootProject.version = "0.7.6"
+rootProject.version = "0.7.7"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
--- src/test/java/com/gradle/enterprise/conventions/customvalueprovider/WatchFilesystemCustomValueProviderIntegrationTest.java
+++ src/test/java/com/gradle/enterprise/conventions/customvalueprovider/WatchFilesystemCustomValueProviderIntegrationTest.java
@@ -9,11 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class WatchFilesystemCustomValueProviderIntegrationTest extends AbstractGradleEnterprisePluginIntegrationTest {
     @ParameterizedTest
     @CsvSource({
-        "true", "false"
+        "true, ENABLED",
+        "false, DISABLED"
     })
-    public void addWatchFsCustomValue(Boolean watchFsEnabled) {
+    public void addWatchFsCustomValue(Boolean watchFsEnabled, String status) {
         succeeds("help", watchFsEnabled ? "--watch-fs" : "--no-watch-fs");
 
-        assertTrue(getConfiguredBuildScan().containsValue("watchFileSystem", watchFsEnabled.toString()));
+        assertTrue(getConfiguredBuildScan().containsValue("watchFileSystem", status));
     }
 }
