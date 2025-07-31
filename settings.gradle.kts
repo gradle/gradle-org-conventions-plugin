cea7e8aa444e37a92be25625bc691fd3000a2866
Andrzej Jordanow <ajordanow@gradle.com>
2025-07-31 15:49:41 +0200

    Dogfood 0.12.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ rootProject.name = "develocity-conventions-plugin"
 
 plugins {
     id("com.gradle.develocity").version("4.0.1")
-    id("io.github.gradle.develocity-conventions-plugin").version("0.11.0")
+    id("io.github.gradle.develocity-conventions-plugin").version("0.12.1")
 }
