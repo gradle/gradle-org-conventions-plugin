5e60677ff91cc7cb17f384491f884bfb6cc08a91
Christoph Obexer <cobexer@gradle.com>
2025-10-02 17:28:50 +0200

    Develocity 4.2.1
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "gradle-org-conventions-plugin"
 
 plugins {
-    id("com.gradle.develocity").version("4.0.1")
+    id("com.gradle.develocity").version("4.2.1")
     id("io.github.gradle.develocity-conventions-plugin").version("0.12.1")
 }
