e65f2b9d4f1dfa15140ca181242a8a11bddf4964
Christoph Obexer <cobexer@gradle.com>
2026-03-13 15:53:27 +0100

    Update build dependencies
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "gradle-org-conventions-plugin"
 
 plugins {
-    id("com.gradle.develocity").version("4.2.2")
-    id("io.github.gradle.develocity-conventions-plugin").version("0.12.1")
+    id("com.gradle.develocity").version("4.3.2")
+    id("io.github.gradle.develocity-conventions-plugin").version("0.13.0")
 }
